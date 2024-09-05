2921bdd3948906f7f79347adb48e43e67eaeb499
Amin Reza Hallaji <125009662+Amin-rh@users.noreply.github.com>
2024-09-05 14:52:04 +0330

    Update guessing_number.py
    Some problems in variable names were solved.

--- guessing_number.py
+++ guessing_number.py
@@ -2,29 +2,29 @@ import random
 
 
 def Guess_Number():
-    Lower_Bound = 1
-    Upper_Bound = 100
+    _Lower_Bound = 1
+    _Upper_Bound = 100
 
-    Number = random.randint(Lower_Bound, Upper_Bound)
+    _Number = random.randint(_Lower_Bound, _Upper_Bound)
 
-    print(f"Guess a number between {Lower_Bound}, {Upper_Bound}.")
+    print(f"Guess a number between {_Lower_Bound}, {_Upper_Bound}.")
 
-    Attempts = 0
+    _Attempts = 0
     while True:
         try:
-            guess = int(input("Enter your number : "))
-            Attempts += 1
+            Guess = int(input("Enter your number : "))
+            _Attempts += 1
 
-            if guess < Number:
+            if Guess < _Number:
                 print("\tYour guess is too low. try again.")
-            elif guess > Number:
+            elif Guess > _Number:
                 print("\tYour guess is too high. try again.")
             else:
                 print(f"\tCongratulation. The correct number was {
-                      Number} and you guessed it in {Attempts} attempts.")
+                      _Number} and you guessed it in {_Attempts} attempts.")
                 break
         except ValueError:
-            print("Please enter a valid integer.")
+            print("\tPlease enter a valid integer.")
 
 
 Guess_Number()
